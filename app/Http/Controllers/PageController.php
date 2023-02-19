0fef8fab74c45c3790706afbda036a0c49d0c454
davidlgabriel <david.g.leal00@gmail.com>
2023-02-19 16:01:14 +0000

    d

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -38,6 +38,14 @@ class PageController extends Controller
         $tours= ToursEs::where('id',$id)->first();
         return view('tours', ['tours' => $tours, 'image1' => $image1, 'image2'=> $image2, 'galeria' => $galeria]);
 
+    }else{
+        $image1= Image::where('id_tour',$id)->where('ordem',1)->first();
+        $image2= Image::where('id_tour',$id)->where('ordem',2)->first();
+        $galeria= Image::where('id_tour',$id)->where('ordem', '>', 2)->get();
+        
+        $tours= ToursEn::where('id',$id)->first();
+        return view('tours', ['tours' => $tours, 'image1' => $image1, 'image2'=> $image2, 'galeria' => $galeria]);
+
     }
     }
     public function estabelecimento(Request $request)
