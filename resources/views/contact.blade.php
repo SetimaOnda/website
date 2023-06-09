8dbf7ef22f8055d3dee84914574707762935dd0c
davidlgabriel <david.g.leal00@gmail.com>
2023-06-09 20:35:02 +0100

    google

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -66,9 +66,33 @@
         }
     }
     </style>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-M3E6CEQJ83"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-M3E6CEQJ83');
+    </script>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KT32JV7');</script>
+    <!-- End Google Tag Manager -->
+
+
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KT32JV7"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <a class="btn book-now desktop open-datepicker-popup" style="display:none;" href="#" data-toggle="modal"
         data-target="#book" title="Book Now"> <span class="icon"><img src="img/calendar.png" height="23" width="20"
                 style="padding-bottom:0.3rem ;" alt="Book"></span>{{__('messages.book_button')}}</a>
--- resources/views/tours.blade.php
+++ resources/views/tours.blade.php
@@ -43,9 +43,33 @@
         letter-spacing: .075em;
     }
     </style>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-M3E6CEQJ83"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-M3E6CEQJ83');
+    </script>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KT32JV7');</script>
+    <!-- End Google Tag Manager -->
+
+
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KT32JV7"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <a class="btn book-now desktop open-datepicker-popup" style="display:none;"
         href="https://fareharbor.com/embeds/book/setimaondaboattrips/?full-items=yes" title="Book Now"> <span
             class="icon"><img src="{{ asset('img/calendar.png')}}" height="23" width="20"
